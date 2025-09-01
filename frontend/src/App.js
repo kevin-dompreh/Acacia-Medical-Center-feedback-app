f3fb542fe2f62f96175190cda8ce712f874b5d1b
Kevin Dompreh <domprehkevin05@gmail.com>
2025-09-01 16:52:52 +0000

    Ongoing changes till the work is done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import FeedbackForm from "./components/FeedbackForm";
 import Login from "./components/Login";
 import Dashboard from "./components/Dashboard";
 import "./styles.css";
+import RegisterAdmin from "./RegisterAdmin";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route path="/" element={<FeedbackForm />} />
         <Route path="/admin" element={<Login />} />
         <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/register-admin" element={<RegisterAdmin />} />
       </Routes>
     </Router>
   );
